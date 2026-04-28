37f75c89992a7295923cd5928bac8a24292ef043
lucaslima2005 <lucaslimaspaula@gmail.com>
2026-04-28 19:41:00 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    // Função que imprime os elementos do array
+
     public static void imprimirArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
@@ -8,10 +8,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // Declarando um array com 5 valores
+      
         int[] numeros = {10, 20, 30, 40, 50};
 
-        // Chamando a função
+    
         imprimirArray(numeros);
     }
 }
